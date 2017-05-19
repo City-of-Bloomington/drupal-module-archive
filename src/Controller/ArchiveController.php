b0ada6a2e3e8dd818387ac9f76ce443cc52f93e5
inghamn <inghamn@bloomington.in.gov>
2017-05-19 10:48:53 -0400

    Routes are now dynamic, based on module settings

--- src/Controller/ArchiveController.php
+++ src/Controller/ArchiveController.php
@@ -10,39 +10,51 @@ use Drupal\Core\Form\FormState;
 
 class ArchiveController extends ControllerBase
 {
-    public function archive($year, $month)
+    public function archive($type, $year, $month, $day)
     {
-        if (!$year) {
-            if (!empty($_GET['year'])) { $year = (int)$_GET['year']; }
-            if (!$year) { $year = (int)date('Y'); }
-        }
-        if (!$month) {
-            if (!empty($_GET['month'])) { $month = (int)$_GET['month']; }
-            if (!$month) { $month = (int)date('m'); }
-        }
+        $year  = (int)$year;
+        $month = (int)$month;
+        $day   = (int)$day;
 
-        $start = new \DateTime("$year-$month-01");
-        $end   = new \DateTime("$year-$month-01");
-        $end->add(new \DateInterval('P1M1D'));
+        if (!$year) { $year = (int)date('Y'); }
+
+        if (!$month) {
+            $date = "$year-01-01";
+            $period = new \DateInterval('P1Y1D');
+        }
+        else {
+            if (!$day) {
+                $date   = "$year-$month-01";
+                $period = new \DateInterval('P1M1D');
+            }
+            else {
+                $date   = "$year-$month-$day";
+                $period = new \DateInterval('P1D');
+            }
+        }
+        $start = new \DateTime($date);
+        $end   = new \DateTime($date);
+        $end->add($period);
 
         $manager = \Drupal::entityTypeManager();
         $query   = \Drupal::entityQuery('node')
+                 ->condition('type',    'news_release')
                  ->condition('created', $start->format('U'), '>=')
                  ->condition('created', $end  ->format('U'), '<')
-                 ->condition('status', 1);
+                 ->condition('status',  1);
 
-        $form_state = new FormState();
-        $form_state->setAlwaysProcess(true);
-        $form_state->setRebuild(true);
-        $form_state->set('year',  $year);
-        $form_state->set('month', $month);
-        $form = \Drupal::formBuilder()->buildForm('Drupal\archive\Form\YearMonthForm', $form_state);
+        #$form_state = new FormState();
+        #$form_state->setAlwaysProcess(true);
+        #$form_state->setRebuild(true);
+        #$form_state->set('year',  $year);
+        #$form_state->set('month', $month);
+        #$form = \Drupal::formBuilder()->buildForm('Drupal\archive\Form\YearMonthForm', $form_state);
 
         return [
             '#theme'   => 'archive_results',
             '#year'    => $year,
             '#start'   => $start,
-            '#form'    => $form,
+            '#form'    => null,
             '#results' => $manager->getViewBuilder('node')->viewMultiple(
                               $manager->getStorage('node')->loadMultiple($query->execute()),
                               'teaser'
